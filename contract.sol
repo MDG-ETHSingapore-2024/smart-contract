7c07287e681f44d56f364e5d9441ee764a6e6b49
Manan Garg <92792271+Coder-Manan@users.noreply.github.com>
2024-09-21 12:09:19 +0530

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,162 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {FunctionsClient} from "@chainlink/contracts@1.2.0/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts@1.2.0/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts@1.2.0/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/resources/link-token-contracts/
+ */
+
+/**
+ * @title GettingStartedFunctionsConsumer
+ * @notice This is an example contract to show how to make HTTP requests using Chainlink
+ * @dev This contract uses hardcoded values and should not be used in production.
+ */
+contract GettingStartedFunctionsConsumer is FunctionsClient, ConfirmedOwner {
+    using FunctionsRequest for FunctionsRequest.Request;
+
+    // State variables to store the last request ID, response, and error
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+    string private baseUrl;
+    address[] verifiedUsers;
+
+    // Custom error type
+    error UnexpectedRequestID(bytes32 requestId);
+
+    // Event to log responses
+    event Response(
+        bytes32 indexed requestId,
+        string data,
+        bytes error
+    );
+
+    // Router address - Hardcoded for Sepolia
+    // Check to get the router address for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    address router = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
+
+    // JavaScript source code
+    // Fetch character name from the Star Wars API.
+    // Documentation: https://swapi.info/people
+    string source =
+        "const baseUrl = args[0]"
+        "const endPoint = args[1];"
+        "const apiResponse = await Functions.makeHttpRequest({"
+        "url: `${baseUrl}/${endPoint}/`"
+        "});"
+        "if (apiResponse.error) {"
+        "throw Error('Request failed');"
+        "}"
+        "const { data } = apiResponse;"
+        "return Functions.encodeString(data);";
+
+    //Callback gas limit
+    uint32 gasLimit = 300000;
+
+    // donID - Hardcoded for Sepolia
+    // Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    bytes32 donID =
+        0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+
+    // State variable to store the returned character information
+    string public character;
+
+    /**
+     * @notice Initializes the contract with the Chainlink router address and sets the contract owner
+     */
+    constructor(string memory _baseUrl) FunctionsClient(router) ConfirmedOwner(msg.sender) {
+        baseUrl = _baseUrl;
+    }
+
+    // modifier so that only verified contracts can access the DBs
+    modifier onlyVerifiedUser {
+        bool verified = false;
+        uint l = verifiedUsers.length;
+        for (uint idx = 0; idx < l; idx++) 
+        {
+            if (verifiedUsers[idx] == msg.sender){
+                verified = true;
+                break;
+            }
+        }
+        require(verified, "Unauthorised access");
+        _;
+    }
+
+    function registerSelf() external payable {
+        require(msg.value >= 100000000);
+        verifiedUsers.push(msg.sender);
+        // now this contract can also use our SDK
+    }
+
+    function unregisterSelf() external onlyVerifiedUser payable {
+        uint l = verifiedUsers.length;
+        uint idx;
+        for (idx = 0; idx < l; idx++) 
+        {
+            if (verifiedUsers[idx] == msg.sender){
+                break;
+            }
+        }
+        verifiedUsers[idx] = verifiedUsers[l - 1];
+        verifiedUsers.pop();
+        payable (msg.sender).transfer(90000000);
+    }
+
+    // TODO: write methods to call the api and do stuff
+    
+    /**
+     * @notice Sends an HTTP request for character information
+     * @param subscriptionId The ID for the Chainlink subscription
+     * @param args The arguments to pass to the HTTP request
+     * @return requestId The ID of the request
+     */
+    function sendRequest(
+        uint64 subscriptionId,
+        string[] calldata args
+    ) internal onlyOwner returns (bytes32 requestId) {
+        require(args.length == 2, "Bad args");
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(source); // Initialize the request with JS code
+        req.setArgs(args); // Set the arguments for the request
+
+        // Send the request and store the request ID
+        s_lastRequestId = _sendRequest(
+            req.encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+
+        return s_lastRequestId;
+    }
+
+    /**
+     * @notice Callback function for fulfilling a request
+     * @param requestId The ID of the request to fulfill
+     * @param response The HTTP response data
+     * @param err Any errors from the Functions request
+     */
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        // just emit a response
+        emit Response(requestId, string(response), err);
+        // if (s_lastRequestId != requestId) {
+        //     revert UnexpectedRequestID(requestId); // Check if request IDs match
+        // }
+        // // Update the contract's state variables with the response and any errors
+        // s_lastResponse = response;
+        // character = string(response);
+        // s_lastError = err;
+
+        // // Emit an event to log the response
+        // emit Response(requestId, character, s_lastResponse, s_lastError);
+    }
+}
